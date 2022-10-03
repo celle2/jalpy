3032863f21d413edbcb1b3b6c4d4322f0051ddce
celle2 <108885778+celle2@users.noreply.github.com>
2022-10-03 11:42:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "jalpy.py",
     version = "0.1",
     py_modules = ['jalpy'],
-    url = "https://github.com/demotomohiro/remocolab",
+    url = "https://github.com/celle2",
     author = "celle2",
     install_requires = ["installCRD"]
 )
